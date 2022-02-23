41e7cdb71a9ea3194b51a72c2c23dd3ccf9d5718
Uzair Iqbal <uzair.qq@outlook.com>
2022-02-23 05:27:48 +0500

    Created Multiple Routes Of CRUD

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,4 +5,16 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "Get Goals" });
 });
 
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Post Goals" });
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `Update Goals With Id: ${req.params.id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `Delete Goals With Id: ${req.params.id}` });
+});
+
 module.exports = router;
