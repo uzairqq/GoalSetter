38b3dcd6f5028ad27f43411f15a8dfb5ba6e6115
Uzair Iqbal <uzair.qq@outlook.com>
2022-02-23 05:38:38 +0500

    Creating a Controller called Get Goals and using it in Routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { getGoals } = require("../controllers/goalController.js");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
-});
+router.get("/", getGoals);
 
 router.post("/", (req, res) => {
   res.status(200).json({ message: "Post Goals" });
