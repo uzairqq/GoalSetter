8b4068c0fd5d67cf0df30261c807a37a1ab84ac5
Uzair Iqbal <uzair.qq@outlook.com>
2022-02-24 16:27:32 +0500

    Connected with Mongodb and mongoose

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const routes = require("./routes/goalRoutes.js");
 const { errorHandler } = require("./middleware/errorMiddleware.js");
+const colors = require("colors");
+const connectDB = require("./config/db.js");
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
