cd1d4eb118e652bb17573fae832b80ecccf8b818
Uzair Iqbal <uzair.qq@outlook.com>
2022-02-24 15:33:02 +0500

    added Express async handler for error handller rather than using try catch

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,14 +1,14 @@
 // @desc Get Goals
 // @route Get /api/goals
 // @access Private
-const getGoals = (req, res) => {
+const getGoals = async (req, res) => {
   res.status(200).json({ message: "Get Goals" });
 };
 
 // @desc Create Goal
 // @route Post /api/goal
 // @access Private
-const setGoal = (req, res) => {
+const setGoal = async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a Text Feild");
@@ -21,14 +21,14 @@ const setGoal = (req, res) => {
 // @desc Update Goal
 // @route Put /api/goal
 // @access Private
-const updateGoal = (req, res) => {
+const updateGoal = async (req, res) => {
   res.status(200).json({ message: `Update Goals With Id: ${req.params.id}` });
 };
 
 // @desc Remove Goal
 // @route Delete /api/goal
 // @access Private
-const deleteGoal = (req, res) => {
+const deleteGoal = async (req, res) => {
   res.status(200).json({ message: `Delete Goals With Id: ${req.params.id}` });
 };
 
