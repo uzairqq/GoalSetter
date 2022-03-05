4c9d35f496a434fadea52c937579e7b3ed182153
Uzair Iqbal <uzair.qq@outlook.com>
2022-03-05 17:08:33 +0500

    Basic Express Server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = 8000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
