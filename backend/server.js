83d4d536b87dc82e4e9d23ea24dc46f371eb432a
Uzair Iqbal <uzair.qq@outlook.com>
2022-03-18 00:35:32 +0500

    Again Starting From Scratch

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const PORT = 8000;
-
-const app = express();
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
